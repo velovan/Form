12a75c1b99dd0b63387a7dd614fddd9ef94f5264
velovan <velovan@users.noreply.github.com>
2021-11-24 14:37:12 +0200

    Update AbstractDao.java

--- SpringMVCForm/src/main/java/springmvc/dao/AbstractDao.java
+++ SpringMVCForm/src/main/java/springmvc/dao/AbstractDao.java
@@ -15,31 +15,31 @@ public abstract class AbstractDao<PK extends Serializable, T> {
 	
 	@SuppressWarnings("unchecked")
 	public AbstractDao(){
-		this.persistentClass =(Class<T>) ((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[1];
+	    this.persistentClass = (Class<T>) ((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[1];
 	}
 	
 	@Autowired
 	private SessionFactory sessionFactory;
 
 	protected Session getSession(){
-		return sessionFactory.getCurrentSession();
+	    return sessionFactory.getCurrentSession();
 	}
 
 	@SuppressWarnings("unchecked")
 	public T getByKey(PK key) {
-		return (T) getSession().get(persistentClass, key);
+	    return (T) getSession().get(persistentClass, key);
 	}
 
 	public void persist(T entity) {
-		getSession().persist(entity);
+	    getSession().persist(entity);
 	}
 
 	public void delete(T entity) {
-		getSession().delete(entity);
+	    getSession().delete(entity);
 	}
 	
 	protected Criteria createEntityCriteria(){
-		return getSession().createCriteria(persistentClass);
+	    return getSession().createCriteria(persistentClass);
 	}
 
 }
