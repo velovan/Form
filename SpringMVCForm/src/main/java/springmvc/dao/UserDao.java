77cb9f7365a81559460f8d3b143ebe55de9d026d
velovan <velovan@users.noreply.github.com>
2022-05-04 11:53:12 +0300

    Update UserDao.java

--- SpringMVCForm/src/main/java/springmvc/dao/UserDao.java
+++ SpringMVCForm/src/main/java/springmvc/dao/UserDao.java
@@ -5,17 +5,12 @@ import java.util.List;
 import springmvc.model.User;
 
 
-public interface UserDao {
+public interface UserDao{
 
 	User findById(int id);
-	
 	User findBySSO(String sso);
-	
 	void save(User user);
-	
 	void deleteBySSO(String sso);
-	
 	List<User> findAllUsers();
-
 }
 
