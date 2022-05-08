b05f520dcabd84f2703e3760bdddc5b088b8bd17
velovan <velovan@users.noreply.github.com>
2022-05-08 20:28:58 +0300

    Update User.java

--- SpringMVCForm/src/main/java/springmvc/model/User.java
+++ SpringMVCForm/src/main/java/springmvc/model/User.java
@@ -18,7 +18,7 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @Table(name="APP_USER")
-public class User {
+public class User{
 
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
@@ -107,7 +107,7 @@ public class User {
 	}
 
 	@Override
-	public int hashCode() {
+	public int hashCode(){
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
@@ -116,7 +116,7 @@ public class User {
 	}
 
 	@Override
-	public boolean equals(Object obj) {
+	public boolean equals(Object obj){
 		if (this == obj)
 			return true;
 		if (obj == null)
@@ -138,12 +138,10 @@ public class User {
 	}
 
 	@Override
-	public String toString() {
+	public String toString(){
 		return "User [id=" + id + ", ssoId=" + ssoId + ", password=" + password
 		    + ", firstName=" + firstName + ", lastName=" + lastName
 		    + ", email=" + email + "]";
 	}
-
-
 	
 }
