9870ea79250fce219a5bfeee2a40a6914848fa25
velovan <velovan@users.noreply.github.com>
2022-05-15 10:08:42 +0300

    Update UserService.java

--- SpringMVCForm/src/main/java/springmvc/service/UserService.java
+++ SpringMVCForm/src/main/java/springmvc/service/UserService.java
@@ -5,20 +5,14 @@ import java.util.List;
 import springmvc.model.User;
 
 
-public interface UserService {
+public interface UserService{
 	
 	User findById(int id);
-	
 	User findBySSO(String sso);
-	
 	void saveUser(User user);
-	
 	void updateUser(User user);
-	
 	void deleteUserBySSO(String sso);
-
 	List<User> findAllUsers(); 
-	
 	boolean isUserSSOUnique(Integer id, String sso);
 
 }
